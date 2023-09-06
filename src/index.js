00b780adb1e6e39062370bfb80e1ac551fb84098
88kdjo <88kdjo@gmail.com>
2023-09-07 00:32:17 +0900

    Learn JSX & Create Book.jsx and Library.jsx

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from './chapter_03/Library';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    // <App />
+    <Library />
 );
 
 // If you want to start measuring performance in your app, pass a function
