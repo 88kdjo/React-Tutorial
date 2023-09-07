d1884b74321a504c3da9af445b8c5c5e07619a2f
88kdjo <88kdjo@gmail.com>
2023-09-07 12:37:39 +0900

    Learn Forms & Create SignUp.jsx

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ import reportWebVitals from './reportWebVitals';
 // import ConfirmButtonClassComponent from './chapter_08/ConfirmButtonClassComponent';
 // import ConfirmButtonFunctionComponent from './chapter_08/ConfirmButtonFunctionComponent';
 // import LandingPage from './chapter_09/LandingPage';
-import AttendanceBook from './chapter_10/AttendanceBook';
+// import AttendanceBook from './chapter_10/AttendanceBook';
+import SignUp from './chapter_11/SignUp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -23,7 +24,8 @@ root.render(
     // <ConfirmButtonClassComponent />
     // <ConfirmButtonFunctionComponent />
     // <LandingPage />
-    <AttendanceBook />
+    // <AttendanceBook />
+    <SignUp />
 );
 
 // setInterval( () => {
