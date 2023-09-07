c2a33a6bf2613f7ab4082e6604e0229d9f58b76f
88kdjo <88kdjo@gmail.com>
2023-09-07 13:23:11 +0900

    Learn Composition vs Inheritance & Create Card.jsx and ProfileCard.jsx

---
+++ src/chapter_13/Card.jsx
@@ -0,0 +1,17 @@
+const Card = (props) => {
+  const { title, backgroundColor, children } = props;
+
+  return (
+    <div style={{margin: 8, padding: 8, borderRadius:8, boxShadow: '0 0 4 gray', backgroundColor: backgroundColor || 'white'}}>
+      {
+        title && <h1>{title}</h1>
+      }
+      {
+        children
+      }
+    </div>
+  );
+
+}
+
+export default Card;
